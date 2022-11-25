456dbcdbdda5583328d08d43184cd2900452a2e6
SyedMahad <mahad.ehsan@gmail.com>
2022-11-26 01:18:38 +0500

    Edited according to my profile

--- js/main.js
+++ js/main.js
@@ -112,6 +112,14 @@
         loop: true,
         items: 1
     });
+
+    $(".slider").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000, //2000ms = 2s;
+        autoplayHoverPause: true,
+    });
+
     
 })(jQuery);
 
